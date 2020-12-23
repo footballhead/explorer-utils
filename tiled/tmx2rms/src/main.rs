9083c610d4e9c36bf319fb4e973bf183b3ca393e
Michael Hitchens <mdhitchens@gmail.com>
2020-12-23 14:32:10 -0500

    Add name map property parsing

--- tiled/tmx2rms/src/main.rs
+++ tiled/tmx2rms/src/main.rs
@@ -7,6 +7,7 @@ const MIN_TILE: i32 = 1;
 
 struct RmsTmxIntermediate {
     tiles: Vec<u8>,
+    name: String,
 }
 
 fn validate_tmx(map: &tmx::Map) -> Option<RmsTmxIntermediate> {
@@ -17,7 +18,14 @@ fn validate_tmx(map: &tmx::Map) -> Option<RmsTmxIntermediate> {
 
     let mut intermediate = RmsTmxIntermediate {
         tiles: Vec::new(),
+        name: String::new(),
     };
+
+    for property in map.properties() {
+        if property.name() == "name" {
+            intermediate.name = property.value().to_string();
+        }
+    }
 
     let mut found_tiles_layer = false;
     for layer in map.layers() {
@@ -68,14 +76,20 @@ fn main() {
     let tmx_file = &args[1];
     let out_file = &args[2];
 
-    let mut rms = rms::Room::new(1, tmx_file);
-
     let tmx = tmx::Map::open(tmx_file).unwrap();
     let tmx = validate_tmx(&tmx);
     if tmx.is_none() {
         return;
     }
     let tmx = tmx.unwrap();
+
+    let room_id = 1;
+    let room_name = if tmx.name.is_empty() {
+        format!("Room #{}", room_id)
+    } else {
+        tmx.name
+    };
+    let mut rms = rms::Room::new(1, &room_name);
 
     for (i, tile) in tmx.tiles.iter().enumerate() {
         let i = i as u32;
